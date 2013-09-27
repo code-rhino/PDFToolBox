35a89cd82883797d8ae1eeb7bca3c4b99f0809c4
Tariq Hook <thook@MacBookPro-C42C0329F0BE-2.local>
2013-09-27 16:17:43 -0400

    updates

--- lib/PDFToolBox/core.rb
+++ lib/PDFToolBox/core.rb
@@ -9,8 +9,23 @@ module PDFToolBox
 	end
 	
 	class Core 
-		def initialize()
+	
+		attr_reader :input_params
+		def initialize(input_params = {})
+			@input_params = input_params
+			@input_params[:qpdf_path] ||= locate_qpdf || "qpdf"
 			#raise MissingLibrary
+		end
+		
+		def self.locate_qpdf
+			@pdftk_location ||= begin
+				auto_path = %x{locate pdftk | grep "/bin/pdftk"}.strip.split("\n").first
+				(auto_path.nil? || auto_path.empty?) ? nil : auto_path
+        	end			
+		end
+		
+		def locate_qpdf
+			self.class.locate_qpdf
 		end
 	end
 	
--- lib/PDFToolBox/qpdf.rb
+++ lib/PDFToolBox/qpdf.rb
@@ -5,6 +5,7 @@ module PDFToolBox
 		attr_reader :core
 		def initialize()
 			@core = Core.new()
+			"Hello World"
 		end
 	end
 end
